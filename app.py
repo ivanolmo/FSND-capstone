22a5d269a56283ec9ef9ac1552a7001a079013e7
Ivan Olmo <ivan.olmo@gmail.com>
2020-02-09 18:12:50 -0600

    Add starting endpoint to retrieve player list

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask_cors import CORS
+from flask import Flask, request, jsonify, abort
+
+from models import Player, PlayerDetails, PlayerStats, TeamStats, setup_db, \
+    db_drop_and_create_all
+
+# db_drop_and_create_all()
+
+app = Flask(__name__)
+setup_db(app)
+CORS(app)
+
+"""
+API endpoints
+"""
+
+
+@app.route('/players', methods=['GET'])
+def get_players():
+    # requires no authentication
+    try:
+        players = Player.query.all()
+
+        if not players:
+            abort(404)
+
+        players = [player.format() for player in players]
+
+        return jsonify({
+            'success': True,
+            'players': players
+        }), 200
+
+    except Exception as error:
+        raise error
+
