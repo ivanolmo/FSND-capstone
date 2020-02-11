94439e31adbc37885620ce95a969841c2966868f
Ivan Olmo <ivan.olmo@gmail.com>
2020-02-10 19:02:46 -0600

    Move code into a create_app function

--- app.py
+++ app.py
@@ -4,43 +4,52 @@ from flask import Flask, request, jsonify, abort
 from models import Player, PlayerDetails, PlayerStats, TeamStats, setup_db, \
     db_drop_and_create_all
 
-from auth.auth import requires_auth
+# from auth.auth import requires_auth
 
 # db_drop_and_create_all()
 
-app = Flask(__name__)
-setup_db(app)
-CORS(app)
 
-"""
-API endpoints
-"""
+def create_app():
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app, resource={r'/api/*': {'origins': '*'}})
 
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,POST,PATCH,DELETE,OPTIONS')
+        return response
 
-@app.route('/', methods=['GET'])
-def index():
-    return jsonify({
-        'success': True,
-        'message': 'Cool, it works'
-    }), 200
-
-
-@app.route('/players', methods=['GET'])
-def get_players():
-    # requires no authentication
-    try:
-        players = Player.query.all()
-
-        if not players:
-            abort(404)
-
-        players = [player.format() for player in players]
-
+    @app.route('/', methods=['GET'])
+    def index():
         return jsonify({
             'success': True,
-            'players': players
+            'message': 'Cool, it works'
         }), 200
 
-    except Exception as error:
-        raise error
+    @app.route('/players', methods=['GET'])
+    def get_players():
+        # requires no authentication
+        try:
+            players = Player.query.all()
 
+            if not players:
+                abort(404)
+
+            players = [player.format() for player in players]
+
+            return jsonify({
+                'success': True,
+                'players': players
+            }), 200
+
+        except Exception as error:
+            raise error
+
+    return app
+
+
+if __name__ == '__main__':
+    create_app()
