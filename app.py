f82b10203b40967b382ec03d5de21650794b394f
Ivan Olmo <ivan.olmo@gmail.com>
2020-02-16 13:50:47 -0600

    Add get_players endpoint to retrieve players

--- app.py
+++ app.py
@@ -1,49 +1,68 @@
 from flask_cors import CORS
 from flask import Flask, request, jsonify, abort
 
-from models import Player, Details, Stats, setup_db, db_drop_and_create_all
+from models import Player, Stats, setup_db, db_drop_and_create_all
 
 # from auth.auth import requires_auth
 
 # db_drop_and_create_all()
 
-app = Flask(__name__)
-setup_db(app)
-CORS(app, resource={r'/api/*': {'origins': '*'}})
+PLAYERS_PER_PAGE = 10
 
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Headers',
-                         'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods',
-                         'GET,POST,PATCH,DELETE,OPTIONS')
-    return response
+def paginate_players(request, selection):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * PLAYERS_PER_PAGE
+    end = start + PLAYERS_PER_PAGE
+
+    players = [player.format() for player in selection]
+    current_players = players[start:end]
+
+    return current_players
 
 
-@app.route('/', methods=['GET'])
-def index():
-    return jsonify({
-        'success': True,
-        'message': 'Cool, it works'
-    }), 200
+def create_app(test_config=None):
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app, resource={r'/api/*': {'origins': '*'}})
 
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,POST,PATCH,DELETE,OPTIONS')
+        return response
 
-@app.route('/players', methods=['GET'])
-def get_players():
-    # requires no authentication
-    try:
-        players = Player.query.all()
-
-        if not players:
-            abort(404)
-
-        players = [player.format() for player in players]
-
+    @app.route('/', methods=['GET'])
+    def index():
+        # placeholder test endpoint
         return jsonify({
             'success': True,
-            'players': players
+            'message': 'Cool, it works'
         }), 200
 
-    except Exception as error:
-        raise error
+    @app.route('/players', methods=['GET'])
+    def get_players():
+        # requires no authentication
+        try:
+            players = Player.query.all()
+
+            if not players:
+                abort(404)
+
+            players = [player.format() for player in players]
+
+            return jsonify({
+                'success': True,
+                'players': players
+            }), 200
+
+        except Exception as error:
+            raise error
+
+    return app
+
+
+if __name__ == '__main__':
+    create_app()
